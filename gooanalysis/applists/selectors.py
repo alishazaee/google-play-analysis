9bea76b9b850cf825c269bc18da7b3a8f9cd0f15
ali <alishazaee@gmail.com>
2023-12-10 20:44:09 +0330

    updated selectors and services

--- gooanalysis/applists/selectors.py
+++ gooanalysis/applists/selectors.py
@@ -5,3 +5,8 @@ from django.db.models import QuerySet
 
 def get_apps():
     return Applications.objects.all()
+
+def get_application_ids():
+    applications = Applications.objects.all()
+    app_ids = [app.app_id for app in applications]
+    return app_ids
