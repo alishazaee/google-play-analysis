c5a8de32932b668f590b4b87eaa11de245f54980
ali <alishazaee@gmail.com>
2023-12-10 21:29:09 +0330

    updated crawling module

--- gooanalysis/applists/playstore_crawl.py
+++ gooanalysis/applists/playstore_crawl.py
@@ -2,16 +2,29 @@ from google_play_scraper import Sort, reviews
 from google_play_scraper import app
 from google_play_scraper import search
 
-def fetch_reviews(* , app_id:str, count:int):
-    result, continuation_token = reviews(
+def fetch_reviews(app_id, count=1000):
+    result, _ = reviews(
         app_id,
-        lang='en',
+        lang='en', 
         country='us', 
-        sort=Sort.NEWEST,
+        sort=Sort.NEWEST,  
         count=count
     )
 
-    return result
+    extracted_reviews = []
+    for review in result:
+        extracted_review = {
+            "review_id": review['reviewId'],
+            "at": review['at'],  
+            "userName": review['userName'],
+            "thumbsUpCount": review['thumbsUpCount'],
+            "score": review['score'], 
+            "content": review['content']
+        }
+        extracted_reviews.append(extracted_review)
+
+    return extracted_reviews
+
 
 def fetch_app_details(app_id):
    
@@ -24,10 +37,11 @@ def fetch_app_details(app_id):
     return {
         "downloads": result.get('installs', 'Unknown'),
         "score": result.get('score', 'Unknown'),
+        "minInstalls": result.get('score', 'Unknown'),
         "total_reviews": result.get('reviews', 'Unknown'),
         "updated_at": result.get('updated', 'Unknown'),
         "version": result.get('version', 'Unknown'),
         "adSupported": result.get('adSupported', 'Unknown'),
-        "rating": result.get('contentRating', 'Unknown')  
+        "ratings": result.get('contentRating', 'Unknown')  
     }
 
